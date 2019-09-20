2f97376e4d009ecce8e02b74453aadc3beb4878a
hewgreen <hewgreen@ebi.ac.uk>
2019-09-20 10:08:06 +0100

    restructuring main run method

--- lib/hca_project_importer.py
+++ lib/hca_project_importer.py
@@ -2,21 +2,18 @@
 gets metadata for a hca project uuid
 '''
 # todo automatically update config with new paths to attributes as HCA schema evolves
-# todo ref attributes are for linking. We need a method to fill these fields. For now they have placeholders only!
-
 
 __author__ = "hewgreen"
 __license__ = "Apache 2.0"
 __date__ = "30/08/2019"
 
 from hca.dss import DSSClient
-import json
 from ingest.api.ingestapi import IngestApi
-import converter_helper_func
-from get_common_model_entity_metadata import fetch_entity_metadata_translation
+import lib.aux_func as aux_func
+from lib.convert_entity import fetch_entity_metadata_translation
 from collections import defaultdict
 import re
-import sys
+import json
 
 def get_dss_generator(hca_project_uuid):
     # files.project_json.provenance.document_id project uuid you want to retreive
@@ -70,7 +67,7 @@ def get_hca_entity_types(ingest_api_url="http://api.ingest.data.humancellatlas.o
             hca_schemas[domainEntity].append(concreteEntity + '_json')
     return hca_schemas
 
-def check_bundle_assumptions(hca_schemas):
+def check_bundle_assumptions(hca_schemas, metadata_files):
     # Atlas cannot handle multi entities in either duplicated non branched DAG or DAG bubbles
     assert 'biomaterial' in hca_schemas, 'HCA no longer have biomaterial schemas!'
     assert len(metadata_files.get('links_json', [])) == 1, 'More than one links.json file in bundle'
@@ -111,39 +108,7 @@ def get_entity_granularity(common_entity_type):
     assert common_entity_type in granularity, "{} is an unrecognised entity type. Add to the granularity dict in code.".format(common_entity_type)
     return granularity.get(common_entity_type)
 
-if __name__ == '__main__':
-
-    # temp params
-
-    # hca_project_uuid = 'cc95ff89-2e68-4a08-a234-480eca21ce79' # WORKING developed on this dataset
-    # hca_project_uuid = '008e40e8-66ae-43bb-951c-c073a2fa6774' # No error
-    # hca_project_uuid = 'abe1a013-af7a-45ed-8c26-f3793c24a1f4' # No error
-    # hca_project_uuid = 'c4077b3c-5c98-4d26-a614-246d12c2e5d7' # No error
-    # hca_project_uuid = '90bd6933-40c0-48d4-8d76-778c103bf545' # No error
-    # hca_project_uuid = '027c51c6-0719-469f-a7f5-640fe57cbece' # No error
-    # hca_project_uuid = 'cddab57b-6868-4be4-806f-395ed9dd635a' # No error
-    hca_project_uuid = '2043c65a-1cf8-4828-a656-9e247d4e64f1' # No error
-    # hca_project_uuid = 'ae71be1d-ddd8-4feb-9bed-24c3ddb6e1ad' # No error
-    # hca_project_uuid = '74b6d569-3b11-42ef-b6b1-a0454522b4a0' # No error
-    # hca_project_uuid = '2b665444-8bef-4df2-8042-2e7b0f4c47b8' # No error, but no dissociation_protocol_json
-    # hca_project_uuid = '091cf39b-01bc-42e5-9437-f419a66c8a45' # No error, but no dissociation_protocol_json
-    # hca_project_uuid = 'e0009214-c0a0-4a7b-96e2-d6a83e966ce0' # No error, but no dissociation_protocol_json
-    # hca_project_uuid = 'a9c022b4-c771-4468-b769-cabcf9738de3' # No error, but no dissociation_protocol_json
-    # hca_project_uuid = 'f86f1ab4-1fbb-4510-ae35-3ffd752d4dfc' # No error, but no dissociation_protocol_json
-
-
-
-
-    # hca_project_uuid = 'f81efc03-9f56-4354-aabb-6ce819c3d414' # TypeError: sequence item 0: expected str instance, NoneType found
-    # hca_project_uuid = '005d611a-14d5-4fbf-846e-571a1f874f70' # TypeError: sequence item 0: expected str instance, NoneType found
-    # hca_project_uuid = '005d611a-14d5-4fbf-846e-571a1f874f70' # TypeError: sequence item 0: expected str instance, NoneType found
-
-    # hca_project_uuid = '8c3c290d-dfff-4553-8868-54ce45f4ba7f' # Exception: Data type not yet supported at this level. Update the parser to include <class 'dict'>
-    # hca_project_uuid = 'f83165c5-e2ea-4d15-a5cf-33f3550bffde'  # Exception: Data type not yet supported at this level. Update the parser to include <class 'dict'>
-    # hca_project_uuid = 'a29952d9-925e-40f4-8a1c-274f118f1f51' # hca.util.exceptions.SwaggerAPIException: None: None (HTTP 502). Details: {"message": "Internal server error"}
-    # hca_project_uuid = 'f8aa201c-4ff1-45a4-890e-840d63459ca2' # AssertionError: This method expects 1 file per bundle. Detected mutiple singlecell_assay entities in bundle ff87cae7-75ec-403d-b505-ae6d816ba424
-
-    translation_config_file = 'etc/mapping_HCA_to_datamodel.json'
+def convert(hca_project_uuid, translation_config_file):
 
     # initialise
     with open(translation_config_file) as f:
@@ -163,10 +128,10 @@ if __name__ == '__main__':
 
         # initialize bundle
 
-        bundle_graph = converter_helper_func.bundle_info(bundle)
+        bundle_graph = aux_func.bundle_info(bundle)
         metadata_files = bundle.get('metadata').get('files')
         metadata_files_by_uuid = get_metadata_files_by_uuid(metadata_files)
-        # check_bundle_assumptions(hca_schemas) # todo turn on after testing
+        # check_bundle_assumptions(hca_schemas, metadata_files) # todo turn on after testing
 
         bundle_links = defaultdict(list)
         translated_bundle_metadata = defaultdict(list)
@@ -251,7 +216,5 @@ if __name__ == '__main__':
             project_translated_output[entity_type] += entities
 
     # temp writing out json for inspection
-    import json
-    with open('temp_out.json', 'w') as f:
+    with open('log/' + hca_project_uuid + '.common_format.json', 'w') as f:
         json.dump(project_translated_output, f)
-    # print(project_translated_output)
