9e0beb9a57e93926b12147f8c728a16a94ddf3ca
hewgreen <hewgreen@ebi.ac.uk>
2019-10-17 15:00:41 +0100

    networkx specific version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     install_requires=[
                 'hca',
                 'hca-ingest',
-                'networkx',
+                'networkx==2.3',
                 'requests',
                 'urllib3'
           ],
